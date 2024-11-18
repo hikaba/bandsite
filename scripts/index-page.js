42db5fbe4ce568fed4b4a75492e5aca2b2426785
Hiba <hibakaawan98@gmail.com>
2024-11-18 13:04:20 -0500

    added responsiveness and functionality to bio page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,3 +1,10 @@
+//array with some comments
+let userComments = [
+    { name: 'Victor Pinto', date: "11/02/2023", comment: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.'},
+    { name: 'Christina Cabrera', date: "10/28/2023", comment: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.' },
+    {name: 'Isaac Tadesse', date: '10/20/2023', comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
+]
+
 
 //gettting the form and display sections
 const commentsForm = document.getElementById("comments__form");
@@ -27,28 +34,32 @@ function handleFormSubmit(event){
 }
 
 function createCommentCard(comment){
+    console.log(comment);
     const commentsDisplayContainer = document.createElement('div');
     commentsDisplayContainer.className = 'comments-display__container';
     //adding the html with the user information into the comments-display container
     commentsDisplayContainer.innerHTML = `
-        <div class="comments-display__icon-wrapper">
-            <div class="comments-display__user-image"></div>
-        </div>
+        <div class="comments-display__content">
+            <div class="comments-display__icon-wrapper">
+                <div class="comments-display__user-image"></div>
+            </div>
       
-        <div class="comments-display__comment">
-            <div class="comments-display__wrapper">
-                <p class="comments-display__content comments-display__content-bold">
-                    ${comment.name}
-                </p>
-                <p class="comments-display__content comments-display__content-light">
-                    ${comment.date}
+            <div class="comments-display__comment">
+                <div class="comments-display__wrapper">
+                    <p class="comments-display__content comments-display__content-bold">
+                        ${comment.name}
+                    </p>
+                    <p class="comments-display__content comments-display__content-light">
+                        ${comment.date}
+                    </p>
+                </div>
+                
+                <p class="comments-display__content">
+                    ${comment.comment}
                 </p>
             </div>
-            
-            <p class="comments-display__content">
-                ${comment.comment}
-            </p>
         </div>
+        <hr class="comments-display__divider">
     `;
 
     //adding the comment to existing commentsDisplay container
@@ -56,7 +67,10 @@ function createCommentCard(comment){
     commentsDisplay.insertBefore(commentsDisplayContainer, commentsDisplay.firstChild);
     commentsForm.reset();
 }
-
+//creating pre-existing cards from array
+for(let i=0; i< userComments.length; i++){
+    createCommentCard(userComments[i]);
+}
 
 
 
